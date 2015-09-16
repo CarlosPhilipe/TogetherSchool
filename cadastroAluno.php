fa9bff32a656d2ca8854db82a5e297d76fd808be
Carlos Philipe Mendes Bahia <carlosphbahia@gmail.com>
2015-09-16 11:39:37 -0400

    layout responsivo

--- cadastroAluno.php
+++ cadastroAluno.php
@@ -1,7 +1,8 @@
 <?php
 if(!empty($listaAluno)){
 foreach($listaAluno as $r ){
-	$funcod=@$r->get('funcod');
+	$funcod=@$r->get('aluno-cod');
+	echo $funcod;
 	$funtel=@$r->get('funtel');
 	$funend=@$r->get('funend');
 	$funnome=@$r->get('funnome');
@@ -17,125 +18,58 @@ function selected( $value, $selected ){
     return $value==$selected ? ' selected="selected"' : '';
 }
 ?>
-<html>
-<head>
-	<style >
-	#input1{
-		
-		float: right;
-		margin-right: 250px;
-		margin-top: -4px;
-		height: 20px;
-		width: 250px;
-		vertical-align: middle;
-		text-align: center;
-		border-color: lightgreen;
-		border-style: outset;		
-	}
-	#input2{
-		
-		margin-left: 140px;
-		width: 250px;
-		vertical-align: middle;
-		text-align: right;
-		
-	}
-	#input3{
-		
-		float: left;
-		margin-left: 10px;
-		width: 100px;
-		vertical-align: middle;
-		text-align: center;
-		border-radius: 10px;
-		border-style: inset;
-		border-color: gray;
-		background: #E6E8FA;
-		
-	}
-	#b2{
-		
-		margin-left: 140px;
-		width: 250px;
-		vertical-align: middle;
-		text-align: center;
-		
-	}
+ <form class="form-horizontal" action='#' method=POST>
+<div class="row">
+<h1 class="text-center"> Cadastro Aluno</h1>
+  <div class="col-sm-9">
+    
+    <div class="row">
+      <div class="col-xs-8 col-sm-6">
+        <label for="inputEmail3" class="col-sm-2 control-label">Código</label>
+    	<div class="col-sm-10">
+      		<input type="email" class="form-control" id="exampleInputName2" name="aluno-cod" placeholder="Código">
+    	</div>
 
-	#select2{
-		
-		float: right;
-		margin-right: 250px;
-		margin-top: -2px;
-		height: 20px;
-		width: 250px;
-		vertical-align: middle;
-		text-align: center;
-		border-color: lightgreen;
-		border-style: outset;		
-	}
+      	</div>
+      	<div class="col-xs-8 col-sm-6">
+        	<label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+    		<div class="col-sm-10">
+      			<input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+    		</div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-8 col-sm-6">
+        <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+    	<div class="col-sm-10">
+      		<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+    	</div>
+
+      	</div>
+      	<div class="col-xs-8  col-sm-6">
+        	<label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+    		<div class="col-sm-10">
+      			<input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+    		</div>
+      </div>
+    </div>
+  </div>
 
 
-	</style>
-</head>
-</html>
-
-
-
-
-<form action='' method=POST>
-<br><br><br>
-<table border width=800 align=center bgcolor="white">
-	<tr>
-		<td colspan=2 align=center>
-			<h1>ALUNO</h1>
-		</td>
-	</tr>
-	<tr>
-		<td>
-		<hr color=lightgray size="3">
-			<b>NOME:</b>
-			<input id="input1" name=funnome value='<?php echo @$funnome;?>' required>
-			<hr color=lightgray size="3">
-			<b>ENDERECO:</b>
-			<input id="input1" name=funnome value='<?php echo @$funnome;?>' required>
-			<hr color=lightgray size="3">
-			<b>SEXO:</b>
-			<input id="input1" name=funnome value='<?php echo @$funnome;?>' required>
-			<hr color=lightgray size="3">
-			<b>DATA NASCIMENTO:</b>
-			<input id="input1" name=funnome value='<?php echo @$funnome;?>' required>
-			<hr color=lightgray size="3">
-			<b>MATRICULA:</b>
-			<input id="input1" name=funnome value='<?php echo @$funnome;?>' required>
-			<hr color=lightgray size="3">
-			<b>TURMA:</b>
-			<select name="te" id="select2">
-    			<option value="">Escolha</option>
-    			<option value="">TCIP1</option>
-    			<option value="">TCIP2</option>
-    			<!-- <option value="masculino"<?php echo selected( 'masculino', $sexo ); ?>>Masculino</option>
-    			<option value="feminino"<?php echo selected( 'feminino', $sexo ); ?>>Feminino</option> -->
-			</select>
-			<hr color=lightgray size="3">
-	<!-- Para cada funcionário o sistema deverá armazenar seu nome, fone, data de admissão e endereço. -->
-	<!-- <tr>
-		<td colspan=2>
-			<b>SITUACAO : </b><?php echo @$sit;?>
-		</td>
-	</tr>-->
-	<tr> 
-		<td>
-			<input id="input3" type=submit  value="Salvar" >
-		
-			<input id="input3" type=reset value="Cancelar" onClick="location.href='index.php'">
-		</td>
-	</tr>
-</table>
-
-<input type=hidden name="op" value="Aluno">
-<input type=hidden name="op2" value="calcAluno">
-<input type=hidden name=funcod value=<?php echo @$funcod;?>>
-
+	<div class="col-sm-9 text-center">
+	    <div class="row">
+		    <div class="col-xs-8 col-sm-6">
+					 <div class="form-group">
+	 			     <div class="col-sm-offset-2 col-sm-8">
+			     	 	<br><button type="submit" class="btn btn-default">Cadastrar</button>
+				     </div>
+					 </div>
+			  </div>
+			
+			</div>
+		</div>
+	</div>
+</div>
 
 </form>
--- index.php
+++ index.php
@@ -1,4 +1,65 @@
-<!doctype html>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <ul class="nav nav-pills">
+  <li role="presentation" class="active"><a href="?acao=tela">Home</a></li>
+
+  <li role="presentation" class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+      Aluno <span class="caret"></span>
+    </a>
+     <ul class="dropdown-menu">
+            <li><a href="?op=Aluno&op2=telaAluno">Inserir</a></li>
+            <li><a href="?op=Aluno&op2=listaAluno">Listar </a></li>
+    </ul>
+  </li>
+
+  <li role="presentation"><a href="#">Profile</a></li>
+  <li role="presentation"><a href="#">Messages</a></li>
+  
+  </li>
+</ul>
+
+
+ <?php
+
+                  $op=@$_REQUEST['op'];
+                  $op2=@$_REQUEST['op2'];
+                  if(!empty($op)){
+                  $url="controller".$op;
+                  require_once("$url.php");
+                  //echo " <hr> $url <br>";
+                  $o=new $url();
+                  $o->$op2($o);
+                }
+      ?>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
+
+
+<!--<!doctype html>
 <html lang=''>
 <head>
    <meta charset='utf-8'>
@@ -9,7 +70,9 @@
    <script src="script.js"></script>
    <title>Perfumaria</title>
 </head>
-<body bgcolor="black">
+<body class="fundo-cor">
+
+
 
 <div id='cssmenu'>
 <ul>
@@ -44,7 +107,6 @@
    </li>
 </ul>
 </div>
- 
 
       <?php
 
@@ -61,3 +123,4 @@
 
 </body>
 </html>
+-->
